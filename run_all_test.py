23bd781ccb1d3da41593672f59aea7a253ee976f
company_zijie <45645999+tiktokman@users.noreply.github.com>
2020-09-09 15:11:52 +0800

    加入__init__文件，解决用例目录加载失败问题

--- run_all_test.py
+++ run_all_test.py
@@ -1,7 +1,7 @@
 import unittest
 
-test_dir = r'D:\android_airtest\case'
-discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py',top_level_dir=r'D:\android_airtest\case')
+test_dir = r'C:\airtest_script\case'
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py',top_level_dir=None)
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
