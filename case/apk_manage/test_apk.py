09bea62feee160b7e02041f9824e82119d29565c
tiktokman <tiktokman@126.com>
2020-11-17 00:36:18 +0800

    为解决，感觉wait函数或者install_app都会锁住线程

--- case/apk_manage/test_apk.py
+++ case/apk_manage/test_apk.py
@@ -25,7 +25,6 @@ sys.path.append(BASE_DIR)
 from init_setting import *
 
 
-from poco.drivers.android.uiautomation import AndroidUiautomationPoco
 
 @pytest.fixture(scope="module",autouse=True)
 def env_init():
@@ -35,21 +34,24 @@ def env_init():
 	yield
 	simple_report(filepath=os.path.realpath(__file__), logpath=apkManage_logdir, logfile=logfile, output=apkManage_output)
 	print("输出报告")
-
+def call():
+	android = Android()
+	android.install_app(filepath=apkpath,replace=True)	
 def allow_install():
-	'''
-	while exists(Template(r"tpl1605455084989.png", threshold=0.8, record_pos=(-0.243, 0.856), resolution=(1080, 2340))):
-		touch(Template(r"tpl1605455084989.png", threshold=0.8, record_pos=(-0.243, 0.856), resolution=(1080, 2340)))
-		print("111111111")
-	'''
-	wait(Template(r"tpl1605455084989.png", threshold=0.8, record_pos=(-0.243, 0.856), resolution=(1080, 2340)),timeout=60)
-	touch(Template(r"tpl1605455084989.png", threshold=0.8, record_pos=(-0.243, 0.856), resolution=(1080, 2340)))
-	print("2222222222")
+
+    wait(Template(r"tpl1605543346543.png", record_pos=(-0.237, 0.858), resolution=(1080, 2340)),interval=1,timeout=20,intervalfunc=call)
+    touch(Template(r"tpl1605543346543.png", record_pos=(-0.237, 0.858), resolution=(1080, 2340)))
 
 
-def installApp():
-	print("3333333")
-	'''
+def continue_install():
+
+
+	t2 = threading.Thread(target=allow_install)
+	t2.setDaemon(True)
+	t2.start()
+
+	#for i in range(100):
+		#print("主线程：%s",i)
 	try:
 		android = Android()
 		android.check_app(apk)
@@ -58,25 +60,7 @@ def installApp():
 		android.install_app(filepath=apkpath,replace=False)
 	except AirtestError:
 		print("当前设备不存在待安装应用，执行覆盖安装")
-		android.install_app(filepath=apkpath,replace=True)
-	'''
-	android = Android()
-	android.check_app(apk)
-
-	print("当前设备已存在待安装应用，执行覆盖安装")
-	android.install_app(filepath=apkpath,replace=False)
-def continue_install():
-	threads = []
-
-	t1 = threading.Thread(target=installApp)
-	threads.append(t1)
-
-	t2 = threading.Thread(target=allow_install)
-	threads.append(t2)
-	t2.setDaemon(True)
-
-	t1.start()
-	t2.start()
+		android.install_app(filepath=apkpath,replace=True)	
 
 
 class TestApk():
