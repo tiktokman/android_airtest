ae93d3020c668d7d3ae251a4aa47f326c7b6a524
company_zijie <45645999+tiktokman@users.noreply.github.com>
2020-11-17 19:29:28 +0800

    完成应用安装用例

--- case/apk_manage/test_apk.py
+++ case/apk_manage/test_apk.py
@@ -34,13 +34,11 @@ def env_init():
 	yield
 	simple_report(filepath=os.path.realpath(__file__), logpath=apkManage_logdir, logfile=logfile, output=apkManage_output)
 	print("输出报告")
-def call():
-	android = Android()
-	android.install_app(filepath=apkpath,replace=True)	
-def allow_install():
 
-    wait(Template(r"tpl1605543346543.png", record_pos=(-0.237, 0.858), resolution=(1080, 2340)),interval=1,timeout=20,intervalfunc=call)
-    touch(Template(r"tpl1605543346543.png", record_pos=(-0.237, 0.858), resolution=(1080, 2340)))
+def allow_install():
+	while True:
+		wait(Template(r"tpl1605543346543.png", record_pos=(-0.237, 0.858), resolution=(1080, 2340)),timeout=60)
+		touch(Template(r"tpl1605543346543.png", record_pos=(-0.237, 0.858), resolution=(1080, 2340)))
 
 
 def continue_install():
@@ -49,19 +47,23 @@ def continue_install():
 	t2 = threading.Thread(target=allow_install)
 	t2.setDaemon(True)
 	t2.start()
-
+	sleep(2)
 	#for i in range(100):
 		#print("主线程：%s",i)
 	try:
+		#install(apkpath)
+		
 		android = Android()
 		android.check_app(apk)
 	
-		print("当前设备已存在待安装应用，执行覆盖安装")
-		android.install_app(filepath=apkpath,replace=False)
+		print("当前设备已存在待安装应用，覆盖安装")
+		android.install_app(filepath=apkpath,replace=True)
+		
 	except AirtestError:
-		print("当前设备不存在待安装应用，执行覆盖安装")
-		android.install_app(filepath=apkpath,replace=True)	
+		print("当前设备不存在待安装应用，非覆盖安装")
+		android.install_app(filepath=apkpath,replace=False)	
 
+	assert android.check_app(apk),"安装成功"
 
 class TestApk():
 
